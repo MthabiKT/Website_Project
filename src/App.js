1540be494dcd8895f847145133dda80e5d409fee
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-03-28 14:33:43 +0300

    add the navBar component to the main app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router} from 'react-router-dom';
+import Navbar from './Components/Navbar';
+// import Home from './components/Home';
+// import About from './components/About';
+// import Projects from './components/Projects';
+// import Contact from './components/Contact';
 
+
+import './App.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        {/* <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+          
+        </Switch> */}
+        
+      </div>
+    </Router>
   );
 }
 
