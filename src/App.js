e8ef9d38fae590f970afd84da564e6bcbb64ca9a
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-03-28 15:28:06 +0300

    add home component and change Switch to Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './Components/Navbar';
-// import Home from './components/Home';
-// import About from './components/About';
-// import Projects from './components/Projects';
-// import Contact from './components/Contact';
+import Home from './Components/Home';
+// import About from './Components/About';
+// import Contact from './Components/Contact';
 
 
 import './App.css';
@@ -13,14 +12,11 @@ function App() {
     <Router>
       <div className="App">
         <Navbar/>
-        {/* <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/contact" component={Contact} />
-          
-        </Switch> */}
-        
+        <Routes>
+         <Route exact path="/" element={<Home/>} />
+         {/* <Route exact path="/about" component={About} />
+         <Route exact path="/contact" component={Contact} /> */}
+        </Routes> 
       </div>
     </Router>
   );
