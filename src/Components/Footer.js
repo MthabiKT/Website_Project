48e83fa2023554a5d203f6ab3b75504fd617baa7
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-03-30 16:52:12 +0300

    update the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,6 +10,10 @@ function Footer() {
        <div className='social-media'>
         <a href='https://www.facebook.com/' target='_blank' rel='noopener noreferrer'><i className='fab fa-facebook-f'></i></a>
         <a href='https://twitter.com/' target='_blank' rel='noopener noreferrer'><i className='fab fa-twitter'></i></a>
+        <a href='https://instagram.com/' target='_blank' rel='noopener noreferrer'><i className='fab fa-instagram'></i></a>
+        <a href='https://github.com/' target='_blank' rel='noopener noreferrer'><i className='fab fa-github'></i></a>
+        <a href='https://medium.com/' target='_blank' rel='noopener noreferrer'><i className='fab fa-medium'></i></a>
+        <a href='https://linkedin.com/' target='_blank' rel='noopener noreferrer'><i className='fab fa-linkedin'></i></a>
       </div>
     </footer>
   );
