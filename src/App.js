7b77fa3cb8bbf9646579cef5721eec088e5465b8
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-03-30 13:03:07 +0300

    add the contact path

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 // import About from './Components/About';
-// import Contact from './Components/Contact';
+import Contact from './Components/Contact';
 import Footer from './Components/Footer'
 
 import './App.css';
 function App() {
   return (
     <Router>
+      <main className="text-gray-400 bg-gray-900 body-font">
       <div className="App">
         <Navbar/>
         <Routes>
          <Route exact path="/" element={<Home/>} />
-         {/* <Route exact path="/about" component={About} />
-         <Route exact path="/contact" component={Contact} /> */}
+         {/* <Route exact path="/about" component={About} /> */}
+         <Route exact path="/contact" element={<Contact/>} />
         </Routes>
         <Footer/> 
       </div>
+      </main>
     </Router>
   );
 }
