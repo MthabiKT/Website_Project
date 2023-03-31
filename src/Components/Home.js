82b9ffc884700251ee1c58284877d69d15af929f
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-03-31 11:22:32 +0300

    update home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import '../CSS/Home.css';
 import ProjectsButton from './Button';
+import profilePic from '../images/img.jpg';
+
 function Home() {
-  return (
+  return (<>
+    <h1 style={{marginTop:20}}>Welcome to my Portfolio</h1>
     <div className="home-container">
-      <h1>Welcome to my website</h1>
-      <p>This is my personal website where I showcase my projects and skills.</p>
-      <ProjectsButton/>
+      <div className="home-content">
+        <div className="home-text">
+          <p>Mauris consequat turpis non elementum tempor. Maecenas tortor elit, interdum a nisl id, accumsan mattis tellus. Nam ullamcorper lorem ut ligula sagittis, ut dapibus libero dapibus. Curabitur nisi diam, varius non risus sit amet, vehicula viverra neque. Sed ut nulla nibh. Fusce quis tempus sem. Fusce ut aliquam velit. Donec dapibus urna felis, vitae venenatis dui dictum vitae. Sed velit lorem, consectetur ac dui nec, efficitur bibendum metus. Pellentesque nunc arcu, congu</p>
+          <ProjectsButton/>
+        </div>
+        <div className="home-image">
+          <img src={profilePic} alt="Profile" style={{width:'70%', height: 'auto'}}/>
+        </div>
+      </div>
     </div>
+    </>
   );
 }
 
