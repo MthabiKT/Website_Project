5409b6a93bde87814caab7d18dcb2e7f6fed16ec
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-03-31 11:22:02 +0300

    add home and contact routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Contact from './Components/Contact';
-import Footer from './Components/Footer'
+import Footer from './Components/Footer';
 
 import './App.css';
 function App() {
-  return (
-    <Router>
-      <main className="text-gray-400 bg-gray-900 body-font">
-      <div className="App">
-        <Navbar/>
+return (
+  <Router>
+    <div className='App'>
+      <Navbar/>
         <Routes>
-         <Route exact path="/" element={<Home/>} />
-         {/* <Route exact path="/about" component={About} /> */}
-         <Route exact path="/contact" element={<Contact/>} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/contact" element={<Contact />} />
         </Routes>
-        <Footer/> 
+        <Footer />
       </div>
-      </main>
     </Router>
   );
 }
