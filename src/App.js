8c7c0c74a9f5c9eee601f765f02dddc6130927ca
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-04-02 17:36:05 +0300

    Add about route to main app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
-
+import About from './Components/About'
 import './App.css';
 function App() {
 return (
@@ -14,6 +14,7 @@ return (
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/about" element={<About />} />
         </Routes>
         <Footer />
       </div>
