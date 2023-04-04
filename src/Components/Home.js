02ee5f5cc1bfd23ae8a5beda596f1b8b681a0d5d
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-04-04 19:06:28 +0300

    add home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import '../CSS/Home.css';
 import ProjectsButton from './Button';
+<<<<<<< Updated upstream
 import profilePic from '../images/img.jpg';
 
+||||||| Stash base
+import profilePic from '../images/2.jpg';
+
+=======
+import profilePic from '../images/2.jpg';
+>>>>>>> Stashed changes
 function Home() {
   return (<>
     <h1 style={{marginTop:20, marginBottom:20,color:'white',fontSize:30}}>Welcome to our Portfolio</h1>
@@ -16,6 +23,12 @@ function Home() {
           <img src={profilePic} alt="Profile" style={{width:'60%', height: 'auto'}}/>
         </div>
       </div>
+<<<<<<< Updated upstream
+||||||| Stash base
+      </div>
+=======
+    </div>
+>>>>>>> Stashed changes
     </>
   );
 }
