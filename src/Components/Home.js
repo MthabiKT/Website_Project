0e6085dcc5ecee08af79d2e7342839aa49622b0e
MthabiKT <93544131+MthabiKT@users.noreply.github.com>
2023-04-04 18:35:00 +0200

    Updated Home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,24 +1,48 @@
 import React from "react";
 import "../CSS/Home.css";
 import ProjectsButton from "./Button";
-import profilePic from "../images/img.jpg";
+import TeamImages from "../images/Team.jpg";
+import Images from "../images/3.jpg";
 
 function Home() {
-  return (<>
-    <h1 style={{marginTop:20}}>Welcome to our Portfolio</h1>
+  return (
     <div className="home-container">
-      <div className="home-content">
-        <div className="home-text">
-          <h3>Hey There! We are Team 4!:</h3>
-          <p>As a team, we believe in the power of collaboration and are committed to creating an inclusive and supportive environment where everyone's ideas and contributions are valued. We pride ourselves on being adaptable and versatile, taking on challenges with enthusiasm and working together to find solutions.</p>
-          <p>We invite you to explore our portfolio and get to know our team. Whether you're interested in learning about our past projects or looking to collaborate on future endeavors, we're excited to connect with you and share our passion for technology.</p>
-          <ProjectsButton/>
+      <section>
+        <div class="container">
+          <div class="row mt-5 jalign-self-center">
+            <div class="col-8 ">
+              <h1>Hey There! We are Team 4!</h1>
+              <p className="m-4 py-5">
+                As a team, we believe in the power of collaboration and are
+                committed to creating an inclusive and supportive environment
+                where everyone's ideas and contributions are valued. We pride
+                ourselves on being adaptable and versatile, taking on challenges
+                with enthusiasm and working together to find solutions.
+              </p>
+            </div>
+            <div class="col-4">
+              <img src={TeamImages} class="card-img-top" alt="..."></img>
+            </div>
+          </div>
         </div>
-        <div className="home-image">
-          <img src={profilePic} alt="Profile" style={{width:'70%', height: 'auto'}}/>
+        <div class="container">
+          <div class="row my-5">
+            <div class="col-6">
+              <img src={Images} class="card-img-top" alt="..."></img>
+            </div>
+            <div class="col-6 align-self-center">
+              <p>
+                We invite you to explore our portfolio and get to know our team.
+                Whether you're interested in learning about our past projects or
+                looking to collaborate on future endeavors, we're excited to
+                connect with you and share our passion for technology.
+              </p>
+              <ProjectsButton />
+            </div>
+          </div>
         </div>
-      </div>
-    </>
+      </section>
+    </div>
   );
 }
 
