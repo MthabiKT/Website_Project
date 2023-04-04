6fab3f8ecd4106864922da97d5b9a15dcdf21fe1
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-04-04 13:55:21 +0300

    improve contact component using bootstrap

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import '../CSS/Contacts.css';
+import { useState } from 'react';
 import DOMPurify from 'dompurify';
+import Social from './Social';
 
-function Contact() {
+const ContactForm = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
@@ -10,105 +10,102 @@ function Contact() {
   const [emailError, setEmailError] = useState('');
   const [messageError, setMessageError] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setNameError('');
-    setEmailError('');
-    setMessageError('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    let isValid = true;
-
-    // Check if name field is empty
-    if (name.trim() === '') {
+    if (!name) {
       setNameError('Please enter your name');
-      isValid = false;
+      return;
+    } else {
+      setNameError('');
     }
 
-    // Check if email field is empty or contains an invalid email address
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (email.trim() === '') {
+    if (!email) {
       setEmailError('Please enter your email');
-      isValid = false;
-    } else if (!emailRegex.test(email.trim())) {
-      setEmailError('Please enter a valid email address');
-      isValid = false;
+      return;
+    } else {
+      setEmailError('');
     }
 
-    // Check if message field is empty
-    if (message.trim() === '') {
+    if (!message) {
       setMessageError('Please enter a message');
-      isValid = false;
+      return;
+    } else {
+      setMessageError('');
     }
 
-    if (isValid) {
-      console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
-      alert("Saved successfully!");
-      setName('');
-      setEmail('');
-      setMessage('');
-    }
+    // Submit the form
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Message:', message);
+
+    setName('');
+    setEmail('');
+    setMessage('');
   };
 
+  return (
+    <div className="container">
+      <h2 className="text-center mb-5">Keep in Touch</h2>
+      <div className="row">
+        <div className="col-lg-6 mb-4">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className={`form-control ${nameError ? 'is-invalid' : ''}`}
+              />
+              {nameError && <div className="invalid-feedback">{DOMPurify.sanitize(nameError)}</div>}
+            </div>
 
-  return (<>
-    <h2>Keep in Touch</h2>
-    <div className='contact-container'>
-  
-    <div className="contact-form-container">
-      
-      <form onSubmit={handleSubmit} className="contact-form">
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="contact-form-input"
-        />
-        {nameError && <div className="error-message">{DOMPurify.sanitize(nameError)}</div>}
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className={`form-control ${emailError ? 'is-invalid' : ''}`}
+              />
+              {emailError && <div className="invalid-feedback">{DOMPurify.sanitize(emailError)}</div>}
+            </div>
 
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="contact-form-input"
-        />
-        {emailError && <div className="error-message">{DOMPurify.sanitize(emailError)}</div>}
+            <div className="form-group">
+              <label htmlFor="message">Message:</label>
+              <textarea
+                id="message"
+                name="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                className={`form-control ${messageError ? 'is-invalid' : ''}`}
+              ></textarea>
+              {messageError && <div className="invalid-feedback">{DOMPurify.sanitize(messageError)}</div>}
+            </div>
 
-        <label htmlFor="message">Message:</label>
-        <textarea
-          id="message"
-          name="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          className="contact-form-message"
-        ></textarea>
-        {messageError && <div className="error-message">{DOMPurify.sanitize(messageError)}</div>}
-
-        <button type="submit" className="contact-form-submit">Submit</button>
-      </form>
-    </div>
-    <div className="contact-map-container">
-    <div className="mapouter">
-      <div className="gmap_canvas">
-        <iframe className='map'
-          width="450"
-          height="450"
-          id="gmap_canvas"
-          src="https://maps.google.com/maps?q=athi river&t=&z=10&ie=UTF8&iwloc=&output=embed"
-          frameborder="0"
-          marginheight="0"
-          marginwidth="0"></iframe>
+            <button type="submit" className="btn btn-primary mt-3">Submit</button>
+          </form>
+        </div>
+        <div className="col-lg-6">
+          <div className="embed-responsive embed-responsive-1by1">
+            <iframe
+              title="Map"
+              className="embed-responsive-item"
+              src="https://maps.google.com/maps?q=athi%20river&t=&z=10&ie=UTF8&iwloc=&output=embed" 
+            height={250}
+            width={250}
+            ></iframe>
+          </div>
+        </div>
       </div>
+      <Social/>
     </div>
-  </div>
-  </div>
-  </>
-);
-}
+  );
+};
 
-export default Contact;
+export default ContactForm;
