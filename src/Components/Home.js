4a0fd04455c6de38be76c06f91fd3796f34e8f72
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-04-04 15:30:35 +0300

    update imports

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import '../CSS/Home.css';
 import ProjectsButton from './Button';
 import profilePic from '../images/2.jpg';
-import Social from './Social'
 function Home() {
   return (<>
     <h1 style={{marginTop:20, marginBottom:20,color:'white',fontSize:30}}>Welcome to our Portfolio</h1>
@@ -11,7 +10,6 @@ function Home() {
         <div className="home-text">
           <h3>Hey There! We are Team 4</h3>
           <ProjectsButton/>
-        
         </div>
         <div className="home-image">
           <img src={profilePic} alt="Profile" style={{width:'60%', height: 'auto'}}/>
