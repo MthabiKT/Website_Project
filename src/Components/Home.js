0fe91676296ebde6229b8236159adc884e1f27e9
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-04-04 19:53:19 +0300

    add Mthabi's work manually since we butchered git

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,33 +1,5 @@
-<<<<<<< HEAD
-||||||| 4e00518
 import React from "react";
-import "../CSS/Home.css";
-import ProjectsButton from "./Button";
-import profilePic from "../images/img.jpg";
-
-function Home() {
-  return (<>
-    <h1 style={{marginTop:20}}>Welcome to our Portfolio</h1>
-    <div className="home-container">
-      <div className="home-content">
-        <div className="home-text">
-          <h3>Hey There! We are Team 4!:</h3>
-          <p>As a team, we believe in the power of collaboration and are committed to creating an inclusive and supportive environment where everyone's ideas and contributions are valued. We pride ourselves on being adaptable and versatile, taking on challenges with enthusiasm and working together to find solutions.</p>
-          <p>We invite you to explore our portfolio and get to know our team. Whether you're interested in learning about our past projects or looking to collaborate on future endeavors, we're excited to connect with you and share our passion for technology.</p>
-          <ProjectsButton/>
-        </div>
-        <div className="home-image">
-          <img src={profilePic} alt="Profile" style={{width:'70%', height: 'auto'}}/>
-        </div>
-      </div>
-    </>
-  );
-}
-
-export default Home;
-=======
-import React from "react";
-import "../CSS/Home.css";
+// import "../CSS/Home.css";
 import ProjectsButton from "./Button";
 import TeamImages from "../images/Team.jpg";
 import Images from "../images/3.jpg";
@@ -75,4 +47,3 @@ function Home() {
 }
 
 export default Home;
->>>>>>> 0e6085dcc5ecee08af79d2e7342839aa49622b0e
