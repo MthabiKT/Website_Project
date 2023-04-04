31451c604a947abbfe8d4fbbd2a116bc11232afb
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-04-04 13:58:35 +0300

    add nav bar color

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
          Portfolio
