a3fb9dd579a775ae25fa8c7e8c37409b8cdf9833
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-04-04 15:15:49 +0300

    add comments to main app

--- src/App.js
+++ src/App.js
@@ -13,17 +13,17 @@ function App() {
   // Main App component which renders other components based on the routes
 
   return (
-    <Router> // Router component to enable client-side routing
-      <div className='App'> // Main App container
-        <Navbar/> // Navbar component which contains navigation links
-        <Routes> // Routes component to handle rendering of components based on URL
-          <Route exact path="/" element={<Home />} /> // Route for Home component
-          <Route exact path="/contact" element={<Contact />} /> // Route for Contact component
-          <Route exact path="/about" element={<About />} /> // Route for About component
-          <Route exact path="/projects" element={<Project/>} /> // Route for Project component
-          <Route exact path="/skills" element={<SkillsBar />} /> // Route for SkillsBar component
+    <Router> {/*Router component to enable client-side routing*/}
+      <div className='App'>  {/* Main App container*/}
+        <Navbar/>  {/* Navbar component which contains navigation links*/}
+        <Routes>  {/* Routes component to handle rendering of components based on URL*/}
+          <Route exact path="/" element={<Home />} /> {/* Route for Home component*/}
+          <Route exact path="/contact" element={<Contact />} /> {/* Route for Contact component*/}
+          <Route exact path="/about" element={<About />} /> {/* Route for About component*/}
+          <Route exact path="/projects" element={<Project/>} /> {/* Route for Project component*/}
+          <Route exact path="/skills" element={<SkillsBar />} /> {/* Route for SkillsBar component*/}
         </Routes>
-        <Footer /> // Footer component
+        <Footer /> {/*Footer component*/}
       </div>
     </Router>
   );
